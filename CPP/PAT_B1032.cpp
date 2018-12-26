6301fc8400b08bf29c3ba1464a8f705769971c6b
Zhang Jixue <43703223+Kantsmile@users.noreply.github.com>
2018-12-26 19:44:11 +0800

    Create PAT_B1032.cpp

---
+++ CPP/PAT_B1032.cpp
@@ -0,0 +1,25 @@
+/*PAT B1032*/
+#include <stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int school[100010] = {0};
+	for(int i = 1;i <= n;i++)
+	{
+		int schID,score;
+		scanf("%d%d",&schID,&score);
+		school[schID] += score;
+	}
+	int MAX = -1,k;
+	for(int i = 1;i <= 10010;i++)
+	{
+		if(school[i]>MAX)
+		{
+			MAX = school[i];
+			k = i;
+		}
+	}
+	printf("%d %d\n",k,MAX);
+	return 0;
+}
