bb3dbc2fb7fc5cb447bf75c97ffe5b0d6d427f49
Zhang Jixue <43703223+Kantsmile@users.noreply.github.com>
2019-01-13 23:21:03 +0800

    Update piPython2.py

--- PY/piPython2.py
+++ PY/piPython2.py
@@ -1,16 +1,15 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import division
 
-####################导入时间模块
+# 导入时间模块
 import time
 
 ################算法根据马青公式计算圆周率####################
 
 number = int(raw_input('请输入想要计算到小数点后的位数n:'))
 
-###############计算当前时间
-time1=time.time()
+# 计算当前时间
+time1 = time.time()
 
 # 多计算10位，防止尾数取舍的影响
 number1 = number+10
@@ -21,15 +20,15 @@ b = 10**number1
 # 求含4/5的首项
 x1 = b*4//5
 # 求含1/239的首项
-x2 = b// -239
+x2 = b // -239
 
 # 求第一大项
 he = x1+x2
-#设置下面循环的终点，即共计算n项
+# 设置下面循环的终点，即共计算n项
 number *= 2
 
-#循环初值=3，末值2n,步长=2
-for i in xrange(3,number,2):
+# 循环初值=3，末值2n,步长=2
+for i in xrange(3, number, 2):
     # 求每个含1/5的项及符号
     x1 //= -25
     # 求每个含1/239的项及符号
@@ -41,14 +40,13 @@ for i in xrange(3,number,2):
 
 # 求出π
 pai = he*4
-#舍掉后十位
+# 舍掉后十位
 pai //= 10**10
 
-############ 输出圆周率π的值
-paistring=str(pai)
-result=paistring[0]+str('.')+paistring[1:len(paistring)]
+# 输出圆周率π的值
+paistring = str(pai)
+result = paistring[0]+str('.')+paistring[1:len(paistring)]
 print result
 
-time2=time.time()
+time2 = time.time()
 print u'总共耗时：' + str(time2 - time1) + 's'
-
